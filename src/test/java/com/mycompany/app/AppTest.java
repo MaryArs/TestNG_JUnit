436ad075b7f872036a1ba40daa8a4ed89b5c414d
Marina Arsitova <mary.arsitova@gmail.com>
2018-04-09 14:10:15 -0700

    Task#4. Config file, positive tests run before negatives.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,20 +5,19 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 
-@Test
+
 public class AppTest extends AppFixture {
 
+    @Test (groups = "positive")
     //***Positive test for Linux Ubuntu. Create file with general name.
     public void CreateGeneralFileTest() throws IOException {
-
         File file = new File(String.valueOf(path), "SimpleFile.txt");
-
         Assert.assertFalse(file.exists());
         Assert.assertTrue(file.createNewFile());
         System.out.println("File is created!");
         file.delete();
     }
-
+    @Test (groups = "positive")
     //***Positive test for Linux Ubuntu. Create file with extension mp3.
     public void CreateAudioFileTest() throws IOException {
         File file = new File(String.valueOf(path),"File.mp3");
@@ -27,8 +26,8 @@ public class AppTest extends AppFixture {
         System.out.println("File is created!");
         file.delete();
     }
-
-     //***Negative test for Linux Ubuntu. Create file with inexistent extension.
+    @Test (groups = "positive")
+     //***Positive test for Linux Ubuntu. Create file with inexistent extension.
     public void CreateFileWithoutExtension() throws IOException {
         File file = new File(String.valueOf(path),"%023Th.??");
         Assert.assertFalse(file.exists());
@@ -37,8 +36,8 @@ public class AppTest extends AppFixture {
         file.delete();
     }
 
+    @Test(groups = "negative", expectedExceptions = {java.io.IOException.class})
      //***Negative test for Linux Ubuntu. Create file with inappropriate symbol "/".
-    @Test(expectedExceptions = {java.io.IOException.class})
     public void CreateInappropriateFileTest() throws IOException {
         File file = new File(String.valueOf(path),"</>*** **");
         Assert.assertFalse(file.exists());
