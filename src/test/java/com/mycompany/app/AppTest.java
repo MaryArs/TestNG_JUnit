ccf7dfe57167863a9c063626fed879ae4f2c1879
Marina Arsitova <mary.arsitova@gmail.com>
2018-04-09 22:47:16 -0700

    @DataProvider for 3 tests.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -32,43 +32,130 @@ public class AppTest extends AppFixture {
             generateRandomFilesName()
             });
         }
-
         return data.iterator();
     }
 
     private Object generateRandomFilesName(){
-        byte[] array = new byte[7]; // length is bounded by 7
-        new Random().nextBytes(array);
-        String generatedString = new String(array, Charset.forName("UTF-8"));
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
         return generatedString + ".txt";
     }
 
-    @Test (groups = "positive")
+    @Test (groups = "positive", dataProvider = "nameAudioFiles")
     //***Positive test for Linux Ubuntu. Create file with extension mp3.
-    public void CreateAudioFileTest() throws IOException {
-        File file = new File(String.valueOf(path),"File.mp3");
-        Assert.assertFalse(file.exists());
-        Assert.assertEquals(true, file.createNewFile());
-        System.out.println("File is created!");
-        file.delete();
-    }
-    @Test (groups = "positive")
-     //***Positive test for Linux Ubuntu. Create file with inexistent extension.
-    public void CreateFileWithoutExtension() throws IOException {
-        File file = new File(String.valueOf(path),"%023Th.??");
+    public void CreateAudioFileTest(String nameAudioFiles) throws IOException {
+        File file = new File(String.valueOf(path),String.valueOf(nameAudioFiles));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(true, file.createNewFile());
         System.out.println("File is created!");
         file.delete();
     }
 
-    @Test(groups = "negative", expectedExceptions = {java.io.IOException.class})
+    @DataProvider
+    public Iterator<Object[]> nameAudioFiles(){
+        List<Object[]> data = new ArrayList<>();
+        for (int i = 0; i<3; i++){
+            data.add(new  Object[]{
+                    generateRandomAudioFilesName()
+            });
+        }
+        return data.iterator();
+    }
+
+    private Object generateRandomAudioFilesName(){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+        return generatedString + ".mp3";
+    }
+
+    @Test (groups = "positive", dataProvider = "nameFilesWithoutExtension")
+     //***Positive test for Linux Ubuntu. Create file with inexistent extension.
+    public void CreateFileWithoutExtension(String nameFilesWithoutExtension) throws IOException {
+        File file = new File(String.valueOf(path),String.valueOf(nameFilesWithoutExtension));
+        Assert.assertFalse(file.exists());
+        Assert.assertEquals(true, file.createNewFile());
+        System.out.println("File is created!");
+        file.delete();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> nameFilesWithoutExtension(){
+        List<Object[]> data = new ArrayList<>();
+        for (int i = 0; i<3; i++){
+            data.add(new  Object[]{
+                    generateRandomWithoutExtensionFilesName()
+            });
+        }
+        return data.iterator();
+    }
+
+    private Object generateRandomWithoutExtensionFilesName(){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+        return generatedString + ".??";
+    }
+
+
+    @Test(groups = "negative", expectedExceptions = {java.io.IOException.class}, dataProvider = "inappropriateNameFiles")
      //***Negative test for Linux Ubuntu. Create file with inappropriate symbol "/".
-    public void CreateInappropriateFileTest() throws IOException {
-        File file = new File(String.valueOf(path),"</>*** **");
+    public void CreateInappropriateFileTest(String inappropriateNameFiles) throws IOException {
+        File file = new File(String.valueOf(path),String.valueOf(inappropriateNameFiles));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(false, file.createNewFile());
         System.out.println("File is not created!");
+    }
+
+    @DataProvider
+    public Iterator<Object[]> inappropriateNameFiles(){
+        List<Object[]> data = new ArrayList<>();
+        for (int i = 0; i<3; i++){
+            data.add(new  Object[]{
+                    generateRandomInappropriateFilesName()
+            });
+        }
+        return data.iterator();
+    }
+
+    private Object generateRandomInappropriateFilesName(){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+        return generatedString + "/" + generatedString;
     }
 }
 
