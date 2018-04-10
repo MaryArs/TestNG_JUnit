5a7d2befa461e11fb86f7961f893251700508221
Marina Arsitova <mary.arsitova@gmail.com>
2018-04-09 23:29:10 -0700

    Task#6.DataProviders.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,28 +13,28 @@ import java.util.Random;
 
 public class AppTest extends AppFixture {
 
-    @Test (groups = "positive", dataProvider = "nameFiles")
+    @Test(groups = "positive", dataProvider = "nameFiles")
     //***Positive test for Linux Ubuntu. Create file with general name.
     public void CreateGeneralFileTest(String nameFiles) throws IOException {
         File file = new File(String.valueOf(path), String.valueOf(nameFiles));
         Assert.assertFalse(file.exists());
-        Assert.assertEquals(true,file.createNewFile());
+        Assert.assertEquals(true, file.createNewFile());
         System.out.println("File is created!");
         file.delete();
     }
 
     @DataProvider
-    public Iterator<Object[]> nameFiles(){
+    public Iterator<Object[]> nameFiles() {
         List<Object[]> data = new ArrayList<>();
-        for (int i = 0; i<3; i++){
-            data.add(new  Object[]{
-            generateRandomFilesName()
+        for (int i = 0; i < 3; i++) {
+            data.add(new Object[]{
+                    generateRandomFilesName()
             });
         }
         return data.iterator();
     }
 
-    private Object generateRandomFilesName(){
+    private Object generateRandomFilesName() {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
@@ -50,10 +49,10 @@ public class AppTest extends AppFixture {
         return generatedString + ".txt";
     }
 
-    @Test (groups = "positive", dataProvider = "nameAudioFiles")
+    @Test(groups = "positive", dataProvider = "nameAudioFiles")
     //***Positive test for Linux Ubuntu. Create file with extension mp3.
     public void CreateAudioFileTest(String nameAudioFiles) throws IOException {
-        File file = new File(String.valueOf(path),String.valueOf(nameAudioFiles));
+        File file = new File(String.valueOf(path), String.valueOf(nameAudioFiles));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(true, file.createNewFile());
         System.out.println("File is created!");
@@ -61,17 +60,17 @@ public class AppTest extends AppFixture {
     }
 
     @DataProvider
-    public Iterator<Object[]> nameAudioFiles(){
+    public Iterator<Object[]> nameAudioFiles() {
         List<Object[]> data = new ArrayList<>();
-        for (int i = 0; i<3; i++){
-            data.add(new  Object[]{
+        for (int i = 0; i < 3; i++) {
+            data.add(new Object[]{
                     generateRandomAudioFilesName()
             });
         }
         return data.iterator();
     }
 
-    private Object generateRandomAudioFilesName(){
+    private Object generateRandomAudioFilesName() {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
@@ -86,64 +85,37 @@ public class AppTest extends AppFixture {
         return generatedString + ".mp3";
     }
 
-    @Test (groups = "positive", dataProvider = "nameFilesWithoutExtension")
-     //***Positive test for Linux Ubuntu. Create file with inexistent extension.
-    public void CreateFileWithoutExtension(String nameFilesWithoutExtension) throws IOException {
-        File file = new File(String.valueOf(path),String.valueOf(nameFilesWithoutExtension));
+    @Test(groups = "positive", dataProviderClass = DataProviders.class, dataProvider = "loadFileNamesFromFile")
+    //***Positive test for Linux Ubuntu. Create file with inexistent extension.
+    public void CreateFileWithoutExtension(String loadFileNamesFromFile) throws IOException {
+        File file = new File(String.valueOf(path), String.valueOf(loadFileNamesFromFile));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(true, file.createNewFile());
         System.out.println("File is created!");
         file.delete();
     }
 
-    @DataProvider
-    public Iterator<Object[]> nameFilesWithoutExtension(){
-        List<Object[]> data = new ArrayList<>();
-        for (int i = 0; i<3; i++){
-            data.add(new  Object[]{
-                    generateRandomWithoutExtensionFilesName()
-            });
-        }
-        return data.iterator();
-    }
-
-    private Object generateRandomWithoutExtensionFilesName(){
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String generatedString = buffer.toString();
-        return generatedString + ".??";
-    }
-
-
     @Test(groups = "negative", expectedExceptions = {java.io.IOException.class}, dataProvider = "inappropriateNameFiles")
-     //***Negative test for Linux Ubuntu. Create file with inappropriate symbol "/".
+    //***Negative test for Linux Ubuntu. Create file with inappropriate symbol "/".
     public void CreateInappropriateFileTest(String inappropriateNameFiles) throws IOException {
-        File file = new File(String.valueOf(path),String.valueOf(inappropriateNameFiles));
+        File file = new File(String.valueOf(path), String.valueOf(inappropriateNameFiles));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(false, file.createNewFile());
         System.out.println("File is not created!");
     }
 
     @DataProvider
-    public Iterator<Object[]> inappropriateNameFiles(){
+    public Iterator<Object[]> inappropriateNameFiles() {
         List<Object[]> data = new ArrayList<>();
-        for (int i = 0; i<3; i++){
-            data.add(new  Object[]{
+        for (int i = 0; i < 3; i++) {
+            data.add(new Object[]{
                     generateRandomInappropriateFilesName()
             });
         }
         return data.iterator();
     }
 
-    private Object generateRandomInappropriateFilesName(){
+    private Object generateRandomInappropriateFilesName() {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
