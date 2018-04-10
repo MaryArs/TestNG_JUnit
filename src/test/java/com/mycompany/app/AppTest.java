1c5d3685cb51825140d53ac140f67045d2c5a5eb
Marina Arsitova <mary.arsitova@gmail.com>
2018-04-09 21:29:46 -0700

    Random string. @DataProvider for test1.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,22 +1,48 @@
 package com.mycompany.app;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 
 public class AppTest extends AppFixture {
 
-    @Test (groups = "positive")
+    @Test (groups = "positive", dataProvider = "nameFiles")
     //***Positive test for Linux Ubuntu. Create file with general name.
-    public void CreateGeneralFileTest() throws IOException {
-        File file = new File(String.valueOf(path), "SimpleFile.txt");
+    public void CreateGeneralFileTest(String nameFiles) throws IOException {
+        File file = new File(String.valueOf(path), String.valueOf(nameFiles));
         Assert.assertFalse(file.exists());
         Assert.assertEquals(true,file.createNewFile());
         System.out.println("File is created!");
         file.delete();
     }
+
+    @DataProvider
+    public Iterator<Object[]> nameFiles(){
+        List<Object[]> data = new ArrayList<>();
+        for (int i = 0; i<3; i++){
+            data.add(new  Object[]{
+            generateRandomFilesName()
+            });
+        }
+
+        return data.iterator();
+    }
+
+    private Object generateRandomFilesName(){
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return generatedString + ".txt";
+    }
+
     @Test (groups = "positive")
     //***Positive test for Linux Ubuntu. Create file with extension mp3.
     public void CreateAudioFileTest() throws IOException {
